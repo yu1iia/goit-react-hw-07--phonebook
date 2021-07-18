0f85c5db3295d904ec0811577ba438e5bbb37963
Yuliia Burko <yuliia.burko@gmail.com>
2021-07-18 17:29:49 +0200

    HW7

--- src/redux/contacts/operations.jsx
+++ src/redux/contacts/operations.jsx
@@ -1,18 +1,18 @@
 import axios from 'axios';
-import { v4 as uuidv4 } from "uuid";
-import { 
-    fetchContactsRequest,
-    fetchContactsSuccess,
-    fetchContactsError,
-    addContactRequest,
-    addContactSuccess,
-    addContactError,
-    deleteContactRequest,
-    deleteContactSuccess,
-    deleteContactError,
-    } from "./actions";
+import { v4 as uuidv4 } from 'uuid';
+import {
+  fetchContactsRequest,
+  fetchContactsSuccess,
+  fetchContactsError,
+  addContactRequest,
+  addContactSuccess,
+  addContactError,
+  deleteContactRequest,
+  deleteContactSuccess,
+  deleteContactError,
+} from './actions';
 
-  axios.defaults.baseURL = 'http://localhost:4040';
+axios.defaults.baseURL = 'http://localhost:3001';
 
 export const fetchContacts = () => async dispatch => {
   dispatch(fetchContactsRequest());
@@ -22,27 +22,24 @@ export const fetchContacts = () => async dispatch => {
     .catch(error => dispatch(fetchContactsError(error)));
 };
 export const addContact = (name, number) => dispatch => {
-    const contact = {
-      id: uuidv4(),
-      name,
-      number,
-    };
-  
-    dispatch(addContactRequest());
-  
-    axios
-      .post('/contacts', contact)
-      .then(({ data }) => dispatch(addContactSuccess(data)))
-      .catch(error => dispatch(addContactError(error)));
+  const contact = {
+    id: uuidv4(),
+    name,
+    number,
   };
-export const deleteContact = id => dispatch => {
-    dispatch(deleteContactRequest());
-  
-    axios
-      .delete(`/contacts/${id}`)
-      .then(() => dispatch(deleteContactSuccess(id)))
-      .catch(error => dispatch(deleteContactError(error)));
-  };
-  
 
-  
+  dispatch(addContactRequest());
+
+  axios
+    .post('/contacts', contact)
+    .then(({ data }) => dispatch(addContactSuccess(data)))
+    .catch(error => dispatch(addContactError(error)));
+};
+export const deleteContact = id => dispatch => {
+  dispatch(deleteContactRequest());
+
+  axios
+    .delete(`/contacts/${id}`)
+    .then(() => dispatch(deleteContactSuccess(id)))
+    .catch(error => dispatch(deleteContactError(error)));
+};
